2502b314c04d10874dd7ff93d24cfa31c2ad4418
sairajkalkundre <sairajkalkundre@gmail.com>
2022-01-28 19:03:15 +0100

    Added Winner check function

--- App.tsx
+++ App.tsx
@@ -8,12 +8,97 @@
  * @format
  */
 
-import React from 'react';
-import {SafeAreaView, StyleSheet, View} from 'react-native';
+import React, {useState} from 'react';
+import {Alert, SafeAreaView, StyleSheet, View} from 'react-native';
 import Tile from './src/components/Tile';
 import {CircleIcon, CrossIcon} from './src/assets/icon/icon';
 
 const App = () => {
+  const [tileState, setTileState] = useState<Number[][]>([
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ]);
+  const [currentPlayer, setCurrentPlayer] = useState<number>(1);
+
+  const showIcon = (row: number, col: number) => {
+    let value = tileState[row][col];
+    switch (value) {
+      case 1:
+        return <CrossIcon />;
+      case -1:
+        return <CircleIcon />;
+      default:
+        return <View />;
+    }
+  };
+
+  const checkWinner = (): number => {
+    let tileArr = tileState.slice();
+    //Row check
+    let check;
+    for (let i = 0; i < 3; i++) {
+      // @ts-ignore
+      check = tileArr[0][i] + tileArr[1][i] + tileArr[2][i];
+      if (check === 3) {
+        return 1;
+      } else if (check === -3) {
+        return -1;
+      }
+    }
+    //Column Check
+    for (let i = 0; i < 3; i++) {
+      // @ts-ignore
+      check = tileArr[i][0] + tileArr[i][1] + tileArr[i][2];
+      if (check === 3) {
+        return 1;
+      } else if (check === -3) {
+        return -1;
+      }
+    }
+
+    //diagonal check
+    // @ts-ignore
+    check = tileArr[0][0] + tileArr[1][1] + tileArr[2][2];
+    if (check === 3) {
+      return 1;
+    } else if (check === -3) {
+      return -1;
+    }
+
+    // @ts-ignore
+    check = tileArr[2][0] + tileArr[1][1] + tileArr[0][2];
+    if (check === 3) {
+      return 1;
+    } else if (check === -3) {
+      return -1;
+    }
+    //No winners
+    return 0;
+  };
+
+  const onTilePress = (row: number, col: number) => {
+    let tileValue = tileState[row][col];
+    if (tileValue) {
+      return;
+    }
+    let playerOne = currentPlayer;
+    let tiles = tileState.slice();
+    tiles[row][col] = playerOne;
+    setTileState(tiles);
+
+    //Swapping players
+    let playerTwo = currentPlayer === 1 ? -1 : 1;
+    setCurrentPlayer(playerTwo);
+
+    //checkWinner
+    let winner = checkWinner();
+    console.log('Winner', winner);
+    if (winner === 1) {
+      Alert.alert('Winner', 'Player 1 won');
+    }
+  };
+
   return (
     <SafeAreaView
       style={{
@@ -21,24 +106,52 @@ const App = () => {
         alignItems: 'center',
         justifyContent: 'center',
       }}>
-      <CrossIcon />
-
-      <CircleIcon />
-
       <View style={{flexDirection: 'row'}}>
-        <Tile props={styles.leftTopTileStyle}></Tile>
-        <Tile props={styles.middleTopTileStyle} />
-        <Tile props={styles.rightTopTileStyle} />
+        <Tile
+          props={styles.leftTopTileStyle}
+          icon={showIcon(0, 0)}
+          onTilePress={() => onTilePress(0, 0)}
+        />
+        <Tile
+          props={styles.middleTopTileStyle}
+          icon={showIcon(0, 1)}
+          onTilePress={() => onTilePress(0, 1)}
+        />
+        <Tile
+          props={styles.rightTopTileStyle}
+          icon={showIcon(0, 2)}
+          onTilePress={() => onTilePress(0, 2)}
+        />
       </View>
       <View style={{flexDirection: 'row'}}>
-        <Tile props={styles.centerLeftTileStyle} />
-        <Tile />
-        <Tile props={styles.centerRightTileStyle} />
+        <Tile
+          props={styles.centerLeftTileStyle}
+          icon={showIcon(1, 0)}
+          onTilePress={() => onTilePress(1, 0)}
+        />
+        <Tile icon={showIcon(1, 1)} onTilePress={() => onTilePress(1, 1)} />
+        <Tile
+          props={styles.centerRightTileStyle}
+          icon={showIcon(1, 2)}
+          onTilePress={() => onTilePress(1, 2)}
+        />
       </View>
       <View style={{flexDirection: 'row'}}>
-        <Tile props={styles.bottomLeftTileStyle} />
-        <Tile props={styles.bottomMiddleTileStyle} />
-        <Tile props={styles.bottomRightTileStyle} />
+        <Tile
+          props={styles.bottomLeftTileStyle}
+          icon={showIcon(2, 0)}
+          onTilePress={() => onTilePress(2, 0)}
+        />
+        <Tile
+          props={styles.bottomMiddleTileStyle}
+          icon={showIcon(2, 1)}
+          onTilePress={() => onTilePress(2, 1)}
+        />
+        <Tile
+          props={styles.bottomRightTileStyle}
+          icon={showIcon(2, 2)}
+          onTilePress={() => onTilePress(2, 2)}
+        />
       </View>
     </SafeAreaView>
   );
--- src/components/Tile/index.tsx
+++ src/components/Tile/index.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
-import {ImageStyle, StyleSheet, View} from 'react-native';
+import {ImageStyle, StyleSheet, TouchableOpacity} from 'react-native';
 import {colors} from '../../utils/colors';
 
 interface TileProps {
   props?: ImageStyle;
+  icon?: JSX.Element;
+  onTilePress?: () => void;
 }
 
 const styles = StyleSheet.create({
-  tile: {height: 100, width: 100, borderWidth: 5, borderColor: colors.grey},
+  tile: {
+    justifyContent: 'center',
+    height: 100,
+    width: 100,
+    borderWidth: 5,
+    borderColor: colors.grey,
+    alignItems: 'center',
+  },
 });
 
-const Tile = ({props}: TileProps) => {
-  return <View style={[styles.tile, props]} />;
+const Tile = ({props, icon, onTilePress}: TileProps) => {
+  return (
+    <TouchableOpacity style={[styles.tile, props]} onPress={onTilePress}>
+      {icon}
+    </TouchableOpacity>
+  );
 };
 
 export default Tile;
